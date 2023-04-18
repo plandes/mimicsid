1e6166fdddda431c6b19f560c8a8d6513ecedf7b
Paul Landes <landes@mailc.net>
2023-04-18 12:44:17 -0500

    remove warnings in new zensols.nlp version

--- src/python/zensols/mimicsid/__init__.py
+++ src/python/zensols/mimicsid/__init__.py
@@ -3,6 +3,16 @@
 """
 __author__ = 'Paul Landes'
 
+
+def _silence_spacy_parser_warnings():
+    import warnings
+    warnings.filterwarnings(
+        'ignore', message='remove_empty_sentences is deprecated.*')
+
+
+_silence_spacy_parser_warnings()
+
+
 from .domain import *
 from .anon import *
 from .app import *
