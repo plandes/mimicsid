3b2fcf00ff25b2844767fbc70d59c08795369176
Paul Landes <landes@mailc.net>
2024-04-06 15:12:35 -0500

    config files: consolidate and normalize

--- src/bin/preprocess.sh
+++ src/bin/preprocess.sh
@@ -2,20 +2,45 @@
 
 BIN=./dist
 CONFIG=config/system.conf
-
+FAST=1
 # don't allow config leak
 export MIMICRC=
 export MIMICSIDRC=
 
-echo "about to delete previous models, contune? (CTRL-C to quit)"
-read
+function confirm() {
+    echo "about to delete previous models, contune? (CTRL-C to quit)"
+    read
+}
 
-echo "deleting old build artifacts and models..."
-rm -rf target model data stage
+function clean() {
+    echo "deleting old build artifacts and models..."
+    rm -rf target model data stage
+}
 
-echo "parsing admissions, notes and docs"
-# TODO: remove maxadms
-$BIN preempt -w 4 -c ${CONFIG} --maxadm 10
+function preempt() {
+    echo "parsing admissions, notes and docs"
+    if [ $FAST -eq 1 ] ; then
+	$BIN preempt -c ${CONFIG} -w 1 --maxadm 10
+    else
+	$BIN preempt -c ${CONFIG} -w 4
+    fi
+}
 
-echo "batching data"
-$BIN batch -c ${CONFIG}
+function batch() {
+    echo "batching data"
+    if [ $FAST -eq 1 ] ; then
+	$BIN batch -c ${CONFIG} --override \
+	     'model_settings.epochs=2,batch_stash.batch_limit=3'
+    else
+	$BIN batch -c ${CONFIG}
+    fi
+}
+
+function main() {
+    confirm
+    clean
+    preempt
+    batch
+}
+
+main
