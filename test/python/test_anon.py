58da192f96d2380e6b1d08443c17be0df81e5c0d
Paul Landes <landes@mailc.net>
2024-08-08 18:31:51 -0500

    cleanup

--- test/python/test_anon.py
+++ test/python/test_anon.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from zensols.config import ImportIniConfig, ImportConfigFactory
 from zensols.persist import Stash
 from zensols.mimic import HospitalAdmission, Note
-from zensols.mimic.regexnote import RadiologyNote, EchoNote
+from zensols.mimic.regexnote import EchoNote
 from zensols.mimicsid import AnnotatedNote
 
 logger = logging.getLogger(__name__)
@@ -48,7 +48,6 @@ class TestAnnotationAccess(unittest.TestCase):
 
     def _test_without_anon(self):
         rad_note, echo_note = self._get_notes('without')
-        #self.assertEqual(RadiologyNote, type(rad_note))
         self.assertEqual(EchoNote, type(echo_note))
         sec = echo_note.sections_by_name['findings'][0]
         should = 'LEFT ATRIUM: Mild LA enlargement'
@@ -62,7 +61,6 @@ class TestAnnotationAccess(unittest.TestCase):
 
     def _test_with_anon(self):
         rad_note, echo_note = self._get_notes('with')
-        #self.assertEqual(RadiologyNote, type(rad_note))
         self.assertEqual(AnnotatedNote, type(echo_note))
         sec = echo_note.sections_by_name['findings'][0]
         should = 'LEFT ATRIUM: Mild LA enlargement'
